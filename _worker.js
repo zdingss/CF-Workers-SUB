0d53f6a7631e36970834613ebd5f82dff711d71a
zdingss <157188875+zdingss@users.noreply.github.com>
2024-04-09 14:27:08 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -81,7 +81,7 @@ export default {
 			<a href="http://nginx.org/">nginx.org</a>.<br/>
 			Commercial support is available at
 			<a href="http://nginx.com/">nginx.com</a>.</p>
-			
+
 			<p><em>Thank you for using nginx.</em></p>
 			</body>
 			</html>
